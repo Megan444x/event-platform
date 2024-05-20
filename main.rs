9e6b679d28491181a48c935d015a8a1cd1133044
Megan444x <161157051+Megan444x@users.noreply.github.com>
2024-05-20 18:53:57 +0300

    update variable names

--- main.rs
+++ main.rs
@@ -4,40 +4,39 @@ use serde::{Deserialize, Serialize};
 use std::env;
 
 #[derive(Serialize, Deserialize)]
-struct EventRegistrationData {
+struct EventInfo {
     name: String,
     attendee_count: u32,
 }
 
-async fn handle_event_registration(form_data: web::Json<EventRegistrationData>) -> impl Responder {
+async fn register_event(event_info: web::Json<EventInfo>) -> impl Responder {
     HttpResponse::Ok().json(format!(
         "Event '{}' with {} attendees was successfully registered.",
-        form_data.name, form_data.attendee_count
+        event_info.name, event_info.attendee_count
     ))
 }
 
-async fn perform_health_check() -> impl Responder {
+async fn health_check() -> impl Responder {
     HttpResponse::Ok().body("Server is up and running!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-    let server_address = env::var("SERVER_URL").unwrap_or_else(|_| "127.0.0.1:8080".to_string());
-
+    let server_address = env::var("SERVER_ADDRESS").unwrap_or_else(|_| "127.0.0.1:8080".to_string());
     HttpServer::new(|| {
         App::new()
             .service(
                 web::resource("/register_event")
-                    .route(web::post().to(handle_event_registration))
+                    .route(web::post().to(register_event))
             )
             .service(
                 web::resource("/health_check")
-                    .route(web::get().to(perform_health_check))
+                    .route(web::get().to(health_check))
             )
     })
     .bind(&server_address)?
-    .workers(4) // Fine-tuning the number of workers may help with performance depending on your deployment environment.
+    .workers(4)
     .run()
     .await
 }
